ce05485c10595d30daabc9e13d3b73be739fae85
ronitrajfr <ronitrajofficial7@gmail.com>
2025-10-03 16:55:42 +0530

    prototype ready

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import chalk from "chalk";
 import { isValidUrl } from "./utils";
 import express from "express";
 import axios from "axios";
+import { redis } from "./utils/redis";
 
 const app = express();
 
@@ -40,8 +41,23 @@ program
       return;
     }
 
-    app.use(async (req, res, next) => {
+    app.use(async (req, res) => {
+      const cacheKey = `${origin}${req.originalUrl}`;
       try {
+        const isCached = !!(await redis.get(cacheKey));
+
+        if (isCached) {
+          try {
+            const cached = await redis.get(cacheKey);
+            const cachedData = JSON.parse(cached as string);
+            res.set("X-Cache", "HIT");
+            console.log("HIT");
+            return res.status(cachedData.status).send(cachedData.data);
+          } catch (error) {
+            console.error(chalk.red("Redis error:"), error);
+            res.status(500).json({ error: "Redis error" });
+          }
+        }
         // Build full URL with query parameters
         const targetUrl = `${origin}${req.path}${
           req.url.includes("?") ? "?" + req.url.split("?")[1] : ""
@@ -52,19 +68,17 @@ program
 
         const response = await axios.get(targetUrl);
 
-        // const response = await fetch(targetUrl, {
-        //   method: req.method,
-        //   headers: headers,
-        //   body:
-        //     req.method !== "GET" && req.method !== "HEAD"
-        //       ? req.body
-        //       : undefined,
-        // });
+        await redis.set(
+          cacheKey,
+          JSON.stringify({ status: response.status, data: response.data })
+        );
+        console.log("MISS");
+        res.set("X-Cache", "MISS");
 
-        res.status(response.status).send(response.data);
+        return res.status(response.status).send(response.data);
       } catch (error) {
         console.error(chalk.red("Proxy error:"), error);
-        res.status(500).json({ error: "Proxy error" });
+        return res.status(500).json({ error: "Proxy error" });
       }
     });
 
